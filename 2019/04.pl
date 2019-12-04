c641abc800238e00fd3c10c48b9d1bd3fb16df0e
Lee Katz - Aspen <gzu2@cdc.gov>
2019-12-04 09:17:06 -0500

    day 4

---
+++ 2019/04.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+my $puzzleInput="172851-675869";
+# Answer: 1660
+
+###############
+
+my($min,$max) = split(/\-/, $puzzleInput);
+
+my $totalPossibilities = 0;
+for(my $possiblePassword = $min; $possiblePassword <= $max; $possiblePassword++){
+  my $couldWork = checkPassword($possiblePassword);
+  $totalPossibilities += $couldWork;
+}
+print "$totalPossibilities\n";
+
+# Returns 0 or 1
+sub checkPassword{
+  my($password) = @_;
+  my $couldWork = 1;
+
+  my $length = length($password);
+  if($length != 6){
+    $couldWork = 0;
+  }
+
+  my $hasAdjacency = 0;
+  my @digit = split(//, $password);
+  for(my $i=1; $i<$length; $i++){
+    if($digit[$i] == $digit[$i-1]){
+      $hasAdjacency = 1;
+    }
+    if($digit[$i] < $digit[$i-1]){
+      $couldWork = 0;
+    }
+  }
+
+  if(!$hasAdjacency){
+    $couldWork = 0;
+  }
+  return $couldWork;
+}
+
